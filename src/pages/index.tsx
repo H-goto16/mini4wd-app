086dad779e38f91d28187356bfe2def3bd8b4b76
H-goto16 <h.goto.engineer@gmail.com>
2023-08-12 20:51:16 +0900

    Feat: add router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
-import BankThroughFront from "@/components/calc/bank-through";
-import Footer from "@/components/common/footer";
-import Header from "@/components/common/header";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const router = useRouter();
+
+  const pushList = [
+    { key: 1, name: "バンクスルー", path: "/bank-through" },
+    { key: 2, name: "モーター", path: "/motor" },
+  ];
   return (
-    <div>
-      <Header />
-      <BankThroughFront />
-      <Footer />
+    <div className="p-4">
+      {pushList.map((item) => (
+        <button
+          key={item.key}
+          className="text-2xl my-4 block"
+          onClick={() => router.push(item.path)}
+        >
+          ・{item.name}
+        </button>
+      ))}
     </div>
   );
 };
